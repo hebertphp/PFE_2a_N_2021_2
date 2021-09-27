d47664e19480901c6d19670f476d425a251767fb
Hebert <hebertphp@gmail.com>
2021-09-27 20:33:41 -0300

    PFE_2a 27/09

--- aula4/script4_031.js
+++ aula4/script4_031.js
@@ -1,9 +1,10 @@
 var a,b;
-a=10;//= pode ser recebe ou atribui
-b=5;
-a+=b;
-document.write("a ="+a);
-document.write("<br>b ="+b);
+a=10;//a=10  = pode ser recebe ou atribui
+b=5;//b=
+b+=a;//b=
+a-=7;//a=
+document.write("a ="+a);//a = 
+document.write("<br>b ="+b);//b =
 /*
 a = 
 b = 
