8010959250ff8eea9ecc1d48ff59c84d0cc47100
Hebert <hebertphp@gmail.com>
2021-10-18 20:49:25 -0300

    PFE_2a 18/10

--- aula4/script4_15.js
+++ aula4/script4_15.js
@@ -2,5 +2,5 @@
 var a,b,res;
 a=9;
 b=10;
-res=!(a<b);
+res=!(a<b);// res= (True)   res= true
 document.write("res = "+res);
--- aula4/script4_16.js
+++ aula4/script4_16.js
@@ -3,5 +3,6 @@ var a,b,c,d;
 a=50;
 b=120;
 c=200;
-d=(a<=b) && (a>=c);
-document.write("d = "+d);
+d=!((a<=b) && (a>=c));//d= !((TRUE) && (FALSE)) // d=!(false)  d=true
+
+document.write("d = "+d);//
