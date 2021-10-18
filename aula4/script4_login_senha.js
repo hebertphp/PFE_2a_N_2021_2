dd10e0a9cd3a4dacada72f9f66ef0e397fde7e9d
Hebert <hebertphp@gmail.com>
2021-10-18 20:56:32 -0300

    PFE_2a 18/10

---
+++ aula4/script4_login_senha.js
@@ -0,0 +1,10 @@
+var login, senha, entrada_login, entrada_senha, res;
+login="hokage";
+senha="1234";
+
+entrada_login=prompt("Digite seu login");
+entrada_senha=prompt("Digite a sua senha");
+
+res=(login == entrada_login) && (senha == entrada_senha);
+
+document.write("res = "+res);
