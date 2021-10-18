9496e0d113699bc96aba26affa1c1e717769da2b
Hebert <hebertphp@gmail.com>
2021-10-18 20:22:29 -0300

    PFE_2a 18/10

--- aula4/script4_novo.js
+++ aula4/script4_novo.js
@@ -1,14 +1,19 @@
 var x,y,z,res;
-x=2021;
-y="2021";
-z=18;
 
-res=(x==y);//
+x=2021;//   número inteiro
+y="2021";// número string
+z=2021;//número inteiro
+
+res=(x>=z);//2021 >= 2021
 
 document.write(res);//
 
 /*
-=      
-==     
-===    
+=      recebe, atribuição
+==     igualdade, verificar apenas os valores
+===    identicos, verifica o valor e o tipo
+<=     menor ou igual
+>=     maior ou igual
+<      menor
+>      maior
 */
