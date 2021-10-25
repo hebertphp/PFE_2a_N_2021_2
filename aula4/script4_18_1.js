5ab767f4985679b40a179c71807e832929d62472
Hebert <hebertphp@gmail.com>
2021-10-25 19:53:36 -0300

    PFE_2a 25/10/21

--- aula4/script4_18_1.js
+++ aula4/script4_18_1.js
@@ -7,3 +7,6 @@ d=(a<=b) ? "joinha" : "esquece";//d=joinha
 e=(a>=c) ? "blz" : "zoado";//e=zoado
 document.write("d = "+d);// d = joinha
 document.write("<hr>e = "+e);// e = zoado
+/*
+ternário: (condição) ? valor_para_condição_Verd : valor_para_condição_Falsa
+*/
--- aula5/script5_01.js
+++ aula5/script5_01.js
@@ -5,3 +5,6 @@ if (a == b) {
   document.write("a = "+a+", b = "+b+"<br>");
   document.write("A variável a é igual a variável b");
 }
+/*
+
+*/
