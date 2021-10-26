612396f8040367ac0acdd667e20e49b84b9422e0
Hebert <hebertphp@gmail.com>
2021-10-25 21:51:21 -0300

    PFE_2a 25/10/21

---
+++ aula5/script5_06_1.js
@@ -0,0 +1,16 @@
+var login, valor;
+
+login=prompt("Digite o login");
+
+if(login=="Jose"){
+    document.write("Bem vindo Jose");
+}
+else if(login=="Maria"){
+    document.write("Bem vinda Maria");
+}
+else if(login=="Joaquina"){
+    document.write("Bem vinda Joaquina");
+}
+else{
+    document.write("Quem é você???");
+}
