199b6629d4cacbcc8c05bdf3e9caf829d515f8b4
Hebert <hebertphp@gmail.com>
2021-11-22 20:34:06 -0300

    PFE_2a 22/11/21

--- aula5/script5_071.js
+++ aula5/script5_071.js
@@ -1,12 +1,12 @@
 var destino;
 destino=prompt("Digite seu destino: ");
 switch (destino) {
-case "São Paulo":
-  document.write("Seja bem vindo a cidade que nunca para!");
-break;
-case "New York":
-  document.write("Seja bem vindo a Big Apple!");
-break;
-default:
-  document.write("Não encontramos seu destino!");
+    case "São Paulo":
+        document.write("Seja bem vindo a cidade que nunca para!");
+    break;
+    case "New York":
+        document.write("Seja bem vindo a Big Apple!");
+    break;
+    default:
+        document.write("Não encontramos seu destino!");
 }
---
+++ aula5/script5_08_1.js
@@ -0,0 +1,6 @@
+var i;
+i=0; // inicialização do contador;
+while (i < 5) {
+ document.write(i + "<br>");
+ i++;
+}
