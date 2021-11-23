7470aaba6bd599726946541f707d8d7db7aca773
Hebert <hebertphp@gmail.com>
2021-11-22 21:09:24 -0300

    PFE_2a 22/11/21

--- aula5/script5_08_1.js
+++ aula5/script5_08_1.js
@@ -1,6 +1,36 @@
 var i;
-i=0; // inicialização do contador;
-while (i < 5) {
+i=2; //valor inicial 
+while (i <= 5) {//condição
  document.write(i + "<br>");
- i++;
+ i++;//contador soma 1
 }
+document.write("<hr>");
+i=7; //valor inicial 
+while (i < 10) {//condição
+ document.write(i + "<br>");
+ i++;//contador soma 1
+}
+document.write("<hr>");
+i=4; //valor inicial 
+while (i >=1) {//condição
+ document.write(i + "<br>");
+ i--;//contador subtrai 1
+}
+document.write("<hr>");
+i=4; //valor inicial 
+while (i >=1) {//condição
+ document.write(i + "<br>");
+ i--;//contador subtrai 1
+}
+/*
+
+Condição só pode ter valor 
+TRUE ou FALSE....... 
+Teste de mesa
+i   (i>=1)     saída  contador: i--
+4   true        4
+3   true        3
+2   true        2
+1   true        1
+0   false
+*/
