8e23e1558c9776839ba3f01af47f279137e59d4a
Hebert <hebertphp@gmail.com>
2021-11-22 22:01:30 -0300

    PFE_2a 22/11/21

--- aula5/script5_11_1.js
+++ aula5/script5_11_1.js
@@ -1,7 +1,25 @@
-var i,j;
-
-for(i=0;i<3;i++){
-    for(j=0;j<4;j++){
-        document.write(i+" "+j+"<br>");
+var i,j,k;
+for(k=0;k<1;k++){//ponteiro da hora
+    for(i=0;i<2;i++){//ponteiro do minuto
+        for(j=0;j<2;j++){//ponteiro do segundo
+            document.write(k+" "+i+" "+j+"<br>");
+        }
     }
-}
+}    
+/*
+Teste de mesa
+i       j
+0       0
+0       1
+0       2
+0       3
+1       0
+1       1
+1       2
+1       3
+2       0
+2       1
+2       2       
+2       3
+
+*/
