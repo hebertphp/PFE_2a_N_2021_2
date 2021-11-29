ba411da245d27b201ff4522e1ec1eed3c836ebed
Hebert <hebertphp@gmail.com>
2021-11-29 20:59:25 -0300

    PFE_2a 29/11/21

---
+++ aula6/script6_02.js
@@ -0,0 +1,7 @@
+var notas=[10, 9.5, 8.75, 7.5, 6];
+//         0    1    2     3   4
+
+var uf=["SP","RJ","ES","MG"];
+
+document.getElementById("dv1").innerHTML=notas[2];
+document.querySelector("#dv1").innerHTML+=uf[3];
