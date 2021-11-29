5a7c8e2033f2cc122024f986156eeeaef96baa43
Hebert <hebertphp@gmail.com>
2021-11-29 20:38:19 -0300

    PFE_2a 29/11/21

--- aula6/script6_01.js
+++ aula6/script6_01.js
@@ -1,8 +1,10 @@
 var x=[10,50,2021,2];
 var uf=["SP","RJ","ES","MG"];
 // document.write(uf);
-document.getElementById("dv1").innerHTML=uf;
 
+document.getElementById("dv1").innerHTML=uf;
+document.getElementById("dv1").style.color="blue";
+document.getElementById("dv1").style.backgroundColor="black";
 // x=10;
 // x=50;
 // x=2021;
