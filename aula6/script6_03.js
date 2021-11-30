b8d4749bb1f3200aba07d5aedfa628962f562887
Hebert <hebertphp@gmail.com>
2021-11-29 21:15:44 -0300

    PFE_2a 29/11/21

--- aula6/script6_03.js
+++ aula6/script6_03.js
@@ -1,5 +1,11 @@
-var x;
-x="oi ";
-x+=", funciona?";
+var x=["oi ",", funciona?"];
+// x="oi ";
+// x=", funciona?";
 
-document.querySelector("#dv1").innerHTML=x;
+document.querySelector("#dv1").innerHTML=x[0]+x[1];
+
+/*
+Para transformar a var x em uma var do tipo array
+quero que a saída seja: oi, funciona?
+
+*/
