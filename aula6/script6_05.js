80a1ac0c55fe0e6c2d93f17ce33cd0484527dc61
Hebert <hebertphp@gmail.com>
2021-11-29 21:43:13 -0300

    PFE_2a 29/11/21

--- aula6/script6_05.js
+++ aula6/script6_05.js
@@ -1,8 +1,11 @@
 var x=[2020,2,"Hoje"];
-x[0]++;
-x[1]=x[0]%2;
-x[2]+=" em dia";
+x[0]++;//2021
+x[1]=x[0]%2;//1
+x[2]+=" em dia";//Hoje em dia
 
+document.querySelector("#dv1").innerHTML=x[0]+"<br>";
+document.querySelector("#dv1").innerHTML+=x[1]+"<br>";
+document.querySelector("#dv1").innerHTML+=x[2]+"<br>";
 /*
 document.write(x[0]+"<br>");
 document.write(x[1]+"<br>");
