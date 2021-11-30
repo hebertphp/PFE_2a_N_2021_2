c29c931f4164b491da8765d6f05b6aa38f693cd8
Hebert <hebertphp@gmail.com>
2021-11-29 21:36:21 -0300

    PFE_2a 29/11/21

---
+++ aula6/script6_04.js
@@ -0,0 +1,10 @@
+var valores=["SP",7.5,2021];
+//            0   1    2
+
+document.querySelector("#dv1").innerHTML=valores[0];
+document.querySelector(".teste").innerHTML=valores[2];
+/*
+imprimir o valor SP na id=dv1
+imprimir o valor 2021 na class=teste
+valores da var valores
+*/
---
+++ aula6/script6_05.js
@@ -0,0 +1,11 @@
+var x=[2020,2,"Hoje"];
+x[0]++;
+x[1]=x[0]%2;
+x[2]+=" em dia";
+
+/*
+document.write(x[0]+"<br>");
+document.write(x[1]+"<br>");
+document.write(x[2]+"<br>");
+imprimir x[0] x[1] x[2] usando querySelector 
+*/
