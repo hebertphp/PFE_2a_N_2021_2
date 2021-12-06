c22276b7cb931459c8ef1dd877f6ba10378e152f
Hebert <hebertphp@gmail.com>
2021-12-06 20:33:39 -0300

    PFE_2a 06/12/21

--- aula6/script6_novo1.js
+++ aula6/script6_novo1.js
@@ -1,3 +1,17 @@
-var nota=[10,8,6,7,9];
-
-document.querySelector(".teste").innerHTML=nota;
+var nota=[10,8,6,7,9];//var do tipo array
+//        0  1 2 3 4
+var x, msg;
+var uf=["SP","RJ","MG","ES"];
+//       0    1    2     3
+x=uf[2];
+switch(x){
+    case "SP":
+        msg="São Paulo";
+    break;
+    case "RJ":
+        msg="Rio de Janeiro";
+    break;
+    default:
+        msg="Valor não encontrado";
+}
+document.querySelector(".teste").innerHTML=x+" - "+msg;
