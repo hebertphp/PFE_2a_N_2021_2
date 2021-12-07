9c7a6871e0e73cf624875cb013d51be1753b1943
Hebert <hebertphp@gmail.com>
2021-12-06 21:53:01 -0300

    PFE_2a 06/12/21

--- aula6/script6_08.js
+++ aula6/script6_08.js
@@ -1,7 +1,8 @@
 var num=[10,20,30,40,50];
 var tam;
 tam=num.length;
-document.write("var num possui "+tam+" valores<hr>");
-document.write("Todos os valores da var num: "+num+"<hr>");
-document.write("Valor da primeira posição de num: "+num[0]+"<hr>");
-document.write("Valor da última posição de num: "+num[tam-1]+"<hr>");
+document.querySelector("#dv1").innerHTML="var num possui "+tam+" valores<hr>";
+document.querySelector("#dv1").innerHTML+="Todos os valores da var num: "+num+"<hr>";
+
+document.querySelector("#dv1").innerHTML+="Valor da primeira posição de num: "+num[0]+"<hr>";
+document.querySelector("#dv1").innerHTML+="Valor da última posição de num: "+num[tam-1]+"<hr>";
