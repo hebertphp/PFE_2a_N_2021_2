e2b29d21babd10949fffe9e47f486485778250ce
Hebert <hebertphp@gmail.com>
2021-12-13 20:31:59 -0300

    PFE_2a 13/12/21

--- aula6/script6_09.js
+++ aula6/script6_09.js
@@ -3,7 +3,8 @@ var i,msg;
 msg="";
 var meses=["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
 
-for(i=0;i<=12;i++){
-    msg+=meses+"<hr>";
+for(i=0;i<meses.length;i++){
+    msg+=meses[i]+"<hr>";
 }
+document.querySelector("#dv1").innerHTML=meses.length;
 document.querySelector(".teste").innerHTML=msg;
