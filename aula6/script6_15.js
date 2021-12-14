170714ba3b04da110bb133ab2d399be1caf87602
Hebert <hebertphp@gmail.com>
2021-12-13 22:03:08 -0300

    PFE_2a 13/12/21

--- aula6/script6_15.js
+++ aula6/script6_15.js
@@ -1,6 +1,6 @@
 var uf=["SP","RJ","AC","TO","MG"];
-document.write(uf+"<hr>");
+document.write(uf+"<hr>");//SP RJ AC TO MG
 uf.sort();
-document.write(uf+"<hr>");
+document.write(uf+"<hr>");//AC,MG,RJ,SP,TO
 uf.reverse();
-document.write(uf+"<hr>");
+document.write(uf+"<hr>");//TO SP RJ MG AC
---
+++ aula6/script6_16.js
@@ -0,0 +1,9 @@
+var z = [   0   ,   1   ,          2     ,        3];
+var m = [[1,2,3],[4,5,6],[7,8,9,10,11,12],["oi","fui","blz",220]];
+
+document.write(m[0][0]+" "+m[0][1]+ " "+m[0][2]+"<br>");
+document.write(m[1][0]+" "+m[1][1]+ " "+m[1][2]+"<br>");
+document.write(m[2][0]+" "+m[2][1]+ " "+m[2][2]+"<br>");
+document.write(m[3][2]+"<br>");
+document.write(m[2][5]+"<br>");
+document.write(m[3][3]+"<br>");
