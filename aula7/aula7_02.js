da6517dfa665e319e34490112dc33d94a29345a7
Hebert <hebertphp@gmail.com>
2021-12-20 20:43:13 -0300

    PFE_2a 20/12/21

--- aula7/aula7_02.js
+++ aula7/aula7_02.js
@@ -1,3 +1,4 @@
 function boas_vindas(nome) {
     document.write("Seja bem vindo(a) <b>"+nome+"</b>!<br>");
 }
+boas_vindas("Abelardo");
--- aula7/aula7_03.js
+++ aula7/aula7_03.js
@@ -6,7 +6,9 @@ function media(nota1,nota2){
 var num=media(8,7);
 
 document.write ("Num = "+num+"<br>");
+document.querySelector("#saida").innerHTML="Num = "+num;
 
-document.write(media(10,5));
+document.write(media(10,6));
+document.querySelector(".d1").innerHTML=media(10,6);
 
-media(9,8);//ninguém
+media(9,8);//para ninguém, para o além
