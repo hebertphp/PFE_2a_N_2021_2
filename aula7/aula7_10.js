8cb1704321bbdc9007bf6f36e9934a94ca7c3a09
Hebert <hebertphp@gmail.com>
2021-12-20 22:01:50 -0300

    PFE_2a 20/12/21

--- aula7/aula7_10.js
+++ aula7/aula7_10.js
@@ -2,15 +2,16 @@ function par_impar(){
     document.write(x);
     if(x%2==0){
         let msg=" PAR<br>";//escopo local/bloco
-        // document.write(msg);
+        document.write(msg);
     }
     else{
         var msg=" ÍMPAR<br>";//escopo local/bloco
-        // document.write(msg);
+        document.write(msg);
     }
-    document.write(msg);
+    // document.write(msg);
 }
-    var x=28;//escopo global
-    par_impar();document.write("<hr>");
-    var x=29;//escopo global
-    par_impar();
+var x=28;//escopo global
+par_impar();document.write("<hr>");
+// document.write(msg);
+var x=29;//escopo global
+par_impar();
---
+++ aula7/aula7_12.js
@@ -0,0 +1,4 @@
+for(let i=0;i<5;i++){
+    document.write(i + " ");
+}
+document.write("<br>Ultimo valor i ="+i);
